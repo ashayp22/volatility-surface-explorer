0799c6e0ef7d34c0794ecc13b61c8b5f0fb0f7af
Ashay Parikh <ashayparikh22@gmail.com>
2024-02-08 10:22:50 -0600

    Add back 2D graphs

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var put_prices;
 var call_strikes;
 var put_strikes;
 var years_to_expiry;
-var names;
 var call_prices;
 var interest_rate = 0.01;
 var dividend_yield = 0.0;
@@ -23,7 +22,6 @@ init().then(() => {
     call_strikes = AAPL_DATA.call_strikes;
     put_strikes = AAPL_DATA.put_strikes;
     years_to_expiry = AAPL_DATA.years_to_expiry;
-    names = AAPL_DATA.names;
     spot = AAPL_DATA.spot;
     time = AAPL_DATA.time;
 
@@ -45,28 +43,33 @@ function handlePlotTypeChange() {
 function handleInterestRateChange() {
     var selectedValue = document.getElementById("interestRate").value;
     interest_rate = parseInt(selectedValue, 10) / 100.0;
-    update();
+    update(true);
 }
 
 function handleDividendYieldChange() {
     var selectedValue = document.getElementById("dividendYield").value;
     dividend_yield = parseInt(selectedValue, 10) / 100.0;
-    update();
+    update(true);
 }
 
 document.getElementById("optionSelect").addEventListener("change", handleOptionTypeChange);
 document.getElementById("interestRate").addEventListener("input", handleInterestRateChange);
 document.getElementById("dividendYield").addEventListener("input", handleDividendYieldChange);
 document.getElementById("plotType").addEventListener("change", handlePlotTypeChange);
-// document.getElementById("see2DButton").addEventListener("click", toggle2D);
+document.getElementById("see2DButton").addEventListener("click", toggle2D);
 
 function toggle2D() {
     shouldPlot2D = !shouldPlot2D;
     document.getElementById("see2DButton").textContent = shouldPlot2D ? "Hide 2D Plots" : "Show 2D Plots";
-    update();
+
+    if (!shouldPlot2D) {
+        document.getElementById("info2d").textContent = '';
+    }
+
+    update(shouldPlot2D);
 }
 
-function update() {
+function update(shouldUpdate2D = false) {
     document.getElementById("interestRateText").textContent = `Interest Rate: ${Math.round(interest_rate * 100)}%`;
     document.getElementById("dividendYieldText").textContent = `Dividend Yield: ${Math.round(dividend_yield * 100)}%`;
 
@@ -104,11 +107,11 @@ function update() {
 
     plot3D(isCall ? call_impl_vol : put_impl_vol, spot, strikes, years_to_expiry, time, plotType);
 
-    if (shouldPlot2D) {
+    if (shouldPlot2D && shouldUpdate2D) {
         plot2D(call_strikes, call_impl_vol, put_strikes, years_to_expiry, put_impl_vol);
     }
 
-    console.log(`${performance.now() - start.toFixed(2)} ms`);
+    console.log(`Performance: ${performance.now() - start.toFixed(2)} ms`);
 }
 
 
